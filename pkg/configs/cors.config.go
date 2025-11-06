7a28f4fda41930fdfda796d3f437876d7be2ba26
Ghug <ptp112004@gmail.com>
2025-11-06 20:58:54 +0700

    fix CORS errors

--- pkg/configs/cors.config.go
+++ pkg/configs/cors.config.go
@@ -1,10 +1,18 @@
 package configs
 
-import "github.com/gofiber/fiber/v2/middleware/cors"
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func CorsConfig() cors.Config {
+	allowOrigins := os.Getenv("ALLOWED_ORIGINS")
+	if allowOrigins == "" {
+		allowOrigins = "http://localhost:3000, https://webbuilderv2.vercel.app, https://basilisk-needed-usually.ngrok-free.app"
+	}
     return cors.Config{
-        AllowOrigins:     "http://localhost:3000, https://webbuilderv2.vercel.app, https://basilisk-needed-usually.ngrok-free.app",
+        AllowOrigins:     allowOrigins,
         AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
         AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
         AllowCredentials: true,
